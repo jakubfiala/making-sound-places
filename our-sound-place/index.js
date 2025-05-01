b44f53793d8f224e3e789782722835647abf4a9d
jakubfiala <j4kub.fiala@gmail.com>
2025-05-01 14:25:53 +0200

    archive

--- our-sound-place/index.js
+++ our-sound-place/index.js
@@ -1,10 +1,11 @@
 import { Loader as MapsAPILoader } from "@googlemaps/js-api-loader";
-import JSON5 from 'json5';
+// import JSON5 from 'json5';
 
 import { initialPosition, initialPOV, persistPosition, persistPOV } from "./position.js";
-import { createViewerSocket } from "./sockets.js";
+// import { createViewerSocket } from "./sockets.js";
 import { Sharawadji } from "./sharawadji/index.js";
-import editor from './editor.js';
+// import editor from './editor.js';
+import archive from "./archive.js";
 
 const ctx = new AudioContext();
 ctx.suspend();
@@ -35,8 +36,8 @@ map.addListener('pov_changed', () => {
   persistPOV({ heading: map.getPov().heading, pitch: map.getPov().pitch });
 });
 
-const sharawadji = new Sharawadji([], map, { debug: true }, ctx);
-const socket = await createViewerSocket(sharawadji);
+const sharawadji = new Sharawadji(archive, map, { debug: true }, ctx);
+// const socket = await createViewerSocket(sharawadji);
 
 const startButton = document.getElementById('start');
 const intro = document.getElementById('intro');
@@ -48,27 +49,27 @@ const initAudio = async () => {
 
 startButton.addEventListener('click', initAudio);
 
-const update = () => {
-  const sounds = editor.state.doc.toString();
+// const update = () => {
+//   const sounds = editor.state.doc.toString();
 
-  try {
-    const parsed = JSON5.parse(sounds);
-    console.log(parsed);
-    socket.send(JSON.stringify({ type: 'update sounds', sounds: parsed }))
-  } catch (err) {
-    console.error(err);
-    alert('This code is not valid JSON, sorry :(');
-  }
-};
+//   try {
+//     const parsed = JSON5.parse(sounds);
+//     console.log(parsed);
+//     socket.send(JSON.stringify({ type: 'update sounds', sounds: parsed }))
+//   } catch (err) {
+//     console.error(err);
+//     alert('This code is not valid JSON, sorry :(');
+//   }
+// };
 
-const updateButton = document.getElementById('update');
+// const updateButton = document.getElementById('update');
 
-updateButton.addEventListener('click', update);
+// updateButton.addEventListener('click', update);
 
-document.addEventListener('keydown', (event) => {
-  console.log(event);
-  if (event.key === 's' && (event.ctrlKey || event.metaKey)) {
-    event.preventDefault();
-    update();
-  }
-})
+// document.addEventListener('keydown', (event) => {
+//   console.log(event);
+//   if (event.key === 's' && (event.ctrlKey || event.metaKey)) {
+//     event.preventDefault();
+//     update();
+//   }
+// })
